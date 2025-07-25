80a0c995b3c412969e37330e2b606be35a60d72e
Kauã Lima <kaualimaa1233@gmail.com>
2025-07-25 11:58:25 -0300

    Começando a trabalhar com o lado do cliente. Model criada!

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from gestao_doces.models import Produto
 
-# Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    endereco = models.TextField()
+    favoritos = models.ManyToManyField(
+        Produto, blank=True, related_name="clientes_que_favoritaram")
